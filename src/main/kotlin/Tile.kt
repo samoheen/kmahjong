6e2fcbd2749236f5b02f0297bdd900d130e8ed53
Sam O'Heen <me@samoheen.org>
2025-02-19 00:20:00 +0300

    Initial commit

---
+++ src/main/kotlin/Tile.kt
@@ -0,0 +1,7 @@
+package org.badpost.kmahjong
+
+class Tile {
+    override fun toString(): String {
+        return ""
+    }
+}
---
+++ src/test/kotlin/TileTest.kt
@@ -0,0 +1,11 @@
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class TileTest {
+
+     @Test
+     fun testToString() {
+         assertTrue(true)
+     }
+}
